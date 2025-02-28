14a2219287801ecf54812748e55f8f169f27dc4d
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-02-28 17:17:29 +0530

    feat: complete image upload functionality for banner and side pages by admin

--- server/index.js
+++ server/index.js
@@ -19,7 +19,8 @@ checkConnection()
 //DB Connection ....done
 //admin login ....done
 //add links to button by admin (3 buttons) ....done
-//add image to banner page by admin ....pending
-//add image to side1 page by admin ....pending
-//add image to side2 page by admin ....pending
-//add image to side3 page by admin ....pending
+//add image to banner page by admin ....done
+//add image to side1 page by admin ....done
+//add image to side2 page by admin ....done
+//add image to side3 page by admin ....done
+//pin match by admin ....pending
